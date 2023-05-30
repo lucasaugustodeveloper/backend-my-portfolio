2761a5091738aefaeebf1cf82261571d88c31346
Lucas Augusto Silva <lucas.augusto5061@gmail.com>
2023-05-30 16:02:14 -0300

    add config show other port for admin in environment of dev

---
+++ config/env/development/admin.js
@@ -0,0 +1,15 @@
+module.exports = ({ env }) => ({
+  host: env('HOST'),
+  port: 8080,
+  auth: {
+    secret: env('ADMIN_JWT_SECRET'),
+  },
+  apiToken: {
+    salt: env('API_TOKEN_SALT'),
+  },
+  transfer: {
+    token: {
+      salt: env('TRANSFER_TOKEN_SALT'),
+    },
+  },
+});
