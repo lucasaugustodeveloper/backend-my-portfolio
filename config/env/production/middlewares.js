5b79747ddcf7c5c9f76cb655990d6f6978d8e4ea
Lucas Augusto Silva <lucas.augusto5061@gmail.com>
2023-05-30 16:25:59 -0300

    update export for middleware production

--- config/env/production/middlewares.js
+++ config/env/production/middlewares.js
@@ -1,20 +1,31 @@
-module.exports = [{
-  name: 'strapi::security',
-  config: {
-    contentSecurityPolicy: {
-      useDefaults: true,
-      directives: {
-        'connect-src': ["'self'", 'https:'],
-        'img-src': ["'self'", 'data:', 'blob:', 'market-assets.strapi.io', 'res.cloudinary.com'],
-        'media-src': [
-          "'self'",
-          'data:',
-          'blob:',
-          'market-assets.strapi.io',
-          'res.cloudinary.com'
-        ],
-        upgradeInsecureRequests: null,
+module.exports = [
+  'strapi::errors',
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::logger',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'market-assets.strapi.io', 'res.cloudinary.com'],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            'res.cloudinary.com'
+          ],
+          upgradeInsecureRequests: null,
+        },
       },
     },
-  },
-}];
+  }
+];
